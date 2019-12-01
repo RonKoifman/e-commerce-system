2309163b686a58bd31528183a7a4940e1a77fc22
akav1sh <roneldg68@gmail.com>
2019-12-01 21:42:26 +0200

    all validations done.

--- ECommerceSystem/Validations.cpp
+++ ECommerceSystem/Validations.cpp
@@ -27,7 +27,6 @@ bool cinTypeCheck()
 		cleanBuffer();
 		return false; // The input does not match the expected type
 	}
-
 	return true;
 }
 
@@ -37,7 +36,7 @@ bool isStringLengthValid(const char* str, unsigned int& len)
 	return (len < MAX_CHARACTERS - 1);
 }
 
-bool areCharactersValid(const char* str)
+bool areUsernameCharactersValid(const char* str)
 {
 	unsigned int i = 0;
 
@@ -49,7 +48,53 @@ bool areCharactersValid(const char* str)
 		}
 		i++;
 	}
+	return true;
+}
 
+bool arePasswordCharactersValid(const char* str)
+{
+	unsigned int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if ((str[i] <  '!') || (str[i] > '~'))  //All character from ! to ~ are allowed (excluding whitespaces and few extra chars).
+		{
+			cout << "Bad character found: '" << str[i] << "'" <<  endl;
+			return false;
+		}
+		i++;
+	}
+	return true;
+}
+
+bool areAddressCharactersValid(const char* str)
+{
+	unsigned int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (!((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] == ' ')))
+		{
+			return false;
+		}
+		i++;
+	}
+
+	return true;
+}
+
+bool isInt(const char* str)
+{
+	unsigned int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+		{
+			return false;
+		}
+		i++;
+	}
 	return true;
 }
 
@@ -68,7 +113,7 @@ void usernameValidation(char* username)
 
 		if (isStringLengthValid(username, len))
 		{
-			if (areCharactersValid(username))
+			if (areUsernameCharactersValid(username))
 				isValid = true;
 			else
 				cout << "Use only a-z, A-Z, 0-9 characters. Try again!" << endl;
@@ -95,13 +140,116 @@ void passwordValidation(char* password)
 		if (isStringLengthValid(password, len))
 		{
 			if (len >= 6)
-				isValid = true;
+				if(arePasswordCharactersValid(password))
+					isValid = true;
 			else
 				cout << "Password is too short. Try again!" << endl;
 		}
 		else
 		{
 			cout << "Password is too long. Try again!" << endl;
+		}
+	}
+}
+
+void countryValidation(char *country) 
+{
+	bool isValid = false;
+	unsigned int len = 0;
+
+	cleanBuffer();
+	while (!isValid)
+	{
+		cout << "Country name(maximum 20 characters, letters only): ";
+		cin.getline(country, MAX_CHARACTERS);
+		cleanBuffer();
+
+		if (isStringLengthValid(country, len))
+		{
+			if (areAddressCharactersValid(country))
+				isValid = true;
+			else
+				cout << "Use only a-z, A-Z or space. Try again!" << endl;
+		}
+		else
+		{
+			cout << "Country name is too long. Try again!" << endl;
+		}
+	}
+}
+
+void cityValidation(char *city)
+{
+	bool isValid = false;
+	unsigned int len = 0;
+
+	cleanBuffer();
+	while (!isValid)
+	{
+		cout << "City name(maximum 20 characters, letters only): ";
+		cin.getline(city, MAX_CHARACTERS);
+		cleanBuffer();
+
+		if (isStringLengthValid(city, len))
+		{
+			if (areAddressCharactersValid(city))
+				isValid = true;
+			else
+				cout << "Use only a-z, A-Z or space. Try again!" << endl;
+		}
+		else
+		{
+			cout << "City name is too long. Try again!" << endl;
+		}
+	}
+}
+
+void streetValidation(char *street)
+{
+	bool isValid = false;
+	unsigned int len = 0;
+
+	cleanBuffer();
+	while (!isValid)
+	{
+		cout << "Street name(maximum 20 characters, letters only): ";
+		cin.getline(street, MAX_CHARACTERS);
+		cleanBuffer();
+
+		if (isStringLengthValid(street, len))
+		{
+			if (areAddressCharactersValid(street))
+				isValid = true;
+			else
+				cout << "Use only a-z, A-Z or space. Try again!" << endl;
+		}
+		else
+		{
+			cout << "Street name is too long. Try again!" << endl;
+		}
+	}
+}
+
+void buildingNumberValidation(unsigned int& buildingNumber)
+{
+	bool isValid = false;
+	char number[MAX_CHARACTERS];
+
+	cleanBuffer();
+	while (!isValid)
+	{
+		cout << "Building number: ";
+		cin.getline(number, MAX_CHARACTERS);
+		cleanBuffer();
+
+		if (isInt(number))
+		{
+			buildingNumber = atoi(number);
+			isValid = true;
+		}
+		else
+		{
+			cout << "Wrong building number, use only 0-9 characters. Try again!" << endl;
 		}
 	}
 }
--- ECommerceSystem/Validations.h
+++ ECommerceSystem/Validations.h
@@ -5,8 +5,15 @@ void cleanBuffer();
 bool cinTypeCheck();
 void usernameValidation(char* username);
 bool isStringLengthValid(const char* str, unsigned int& length);
-bool areCharactersValid(const char* str);
+bool areUsernameCharactersValid(const char* str);
+bool arePasswordCharactersValid(const char* str);
+bool areAddressCharactersValid(const char* str);
+bool isInt(const char* str);
 void passwordValidation(char* password);
-void getInput(char* str);
+void countryValidation(char *country);
+void cityValidation(char *city);
+void streetValidation(char *street);
+void buildingNumberValidation(unsigned int& buildingNumber);
+
 
 #endif // __VALIDATIONS_H
